c49b61e3bc86ae172831c6bb0947f117d0602e9b
pohy <0ronon0@gmail.com>
2024-02-05 22:49:14 +0100

    Add basic SDL window ^-^

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include "SDL2/SDL.h"
 
 int main() {
-  // Test test;
-  // test.hello();
-  SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+	SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, 0);
 
-  return 0;
+	bool should_quit = false;
+
+	while (!should_quit) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				should_quit = true;
+			}
+
+			if (event.type == SDL_KEYUP) {
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					should_quit = true;
+				}
+			}
+		}
+	}
+
+	return 0;
 }
